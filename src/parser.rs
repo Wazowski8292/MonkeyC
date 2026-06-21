0a22aa2fc9a553de7eec27a1179d2652f0d2abe0
Wazowski8292 <196363069+Wazowski8292@users.noreply.github.com>
2026-06-22 01:27:28 +0200

    Simplefide the code

--- src/parser.rs
+++ src/parser.rs
@@ -10,50 +10,35 @@ enum Block {
 
 pub fn parse_text(file_path: &String){
     let file = File::open(file_path).expect("Failed to open the file");
-        
     let reader = BufReader::new(file);
 
     let mut stack: Vec<Vec<Block>> = vec![vec![]];
-
     let mut current_multiple_items: Vec<String> = vec![];
+    let mut current_line: String = Default::default();
+
+    let mut first_char = false;
 
     for line in reader.lines().enumerate() {
         let line_content = line.1.expect("Failed to read line");
-        let mut current_line: String = Default::default();
-        let mut first_char = false;
+        first_char = false;
 
         for letters in line_content.chars() {
             match letters {
                 '{' => {
-                    if !current_line.is_empty() {
-                        current_multiple_items.push(current_line.clone());
-                        current_line.clear();
-                        first_char = false;
-                    }
-                    if !current_multiple_items.is_empty() { 
-                        stack.last_mut().unwrap().push(Block::Multiple(current_multiple_items.clone()));
-                        current_multiple_items.clear();
-                    }
+                    add_last_block(&mut first_char, &mut current_line, &mut current_multiple_items, &mut stack);
+
                     stack.push(vec![]);
                 }
                 '}' => {
-                    if !current_line.is_empty() {
-                        current_multiple_items.push(current_line.clone());
-                        current_line.clear();
-                        first_char = false;
-                    }
-                    if !current_multiple_items.is_empty() {
-                        stack.last_mut().unwrap().push(Block::Multiple(current_multiple_items.clone()));
-                        current_multiple_items.clear();
-                    }
+                    add_last_block(&mut first_char, &mut current_line, &mut current_multiple_items, &mut stack);
+
                     if stack.len() > 1 {
                         let finished = stack.pop().unwrap();
                         stack.last_mut().unwrap().push(Block::Colection(finished));
                     }
                 }
                 ';' => { current_multiple_items.push(current_line.clone()); current_line.clear() }
-                '\t' => {}
-                ' ' => {
+                '\t' | ' ' => {
                     if first_char {
                         current_line.push(letters);
                     }
@@ -62,8 +47,10 @@ pub fn parse_text(file_path: &String){
             }
         }
 
+        // Maybe take this out. The dif is if it is mandatory to add ; at th eend or not
         if !current_line.is_empty() {
-            current_multiple_items.push(current_line);
+            current_multiple_items.push(current_line.clone());
+            current_line.clear();
         }
     }
 
@@ -75,4 +62,17 @@ pub fn parse_text(file_path: &String){
     if !current_collection.is_empty() {
         println!("{:#?}", current_collection);
     }
+}
+
+fn add_last_block(first_char: &mut bool, current_line: &mut String, current_multiple_items: &mut Vec<String>, stack: &mut Vec<Vec<Block>>) {
+    if !current_line.is_empty() {
+        current_multiple_items.push(current_line.clone());
+        current_line.clear();
+        *first_char = false;
+    }
+    
+    if !current_multiple_items.is_empty() { 
+        stack.last_mut().unwrap().push(Block::Multiple(current_multiple_items.clone()));
+        current_multiple_items.clear();
+    }   
 }
