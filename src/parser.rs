14ed4475c3241e43a5e7d23a4b3f08aa184ac503
Wazowski8292 <196363069+Wazowski8292@users.noreply.github.com>
2026-06-25 16:54:35 +0200

    Function now have actual code link to them

--- src/parser.rs
+++ src/parser.rs
@@ -25,22 +25,24 @@ pub fn parse_text(file_path: &String) -> Result<Vec<Block>, String> {
         first_char = false;
         let num = num + 1;
         let mut char_pos = 0;
+        let mut skip = false; 
 
         for letters in line_content.chars() {
             char_pos += 1;
 
             match letters {
-                '{' => {
-                    let error = add_last_block(&mut first_char, &mut current_word, &mut current_line, &mut current_multiple_items, &mut stack, num, char_pos);
+                '{' | '(' => {
+                    let error = add_last_block(&mut first_char, &mut current_word, &mut current_line, &mut current_multiple_items, &mut stack, num, char_pos, letters == '(');
                     match error {
                         Err(msg) => {return Err(msg); },
                         _ => {}
                     }
 
                     stack.push(vec![]);
+                    skip = false;
                 }
-                '}' => {
-                    let error = add_last_block(&mut first_char, &mut current_word, &mut current_line, &mut current_multiple_items, &mut stack, num, char_pos);
+                '}' | ')' => {
+                    let error = add_last_block(&mut first_char, &mut current_word, &mut current_line, &mut current_multiple_items, &mut stack, num, char_pos, letters == ')');
                     match error {
                         Err(msg) => {return Err(msg); },
                         _ => {}
@@ -50,6 +52,7 @@ pub fn parse_text(file_path: &String) -> Result<Vec<Block>, String> {
                         let finished = stack.pop().unwrap();
                         stack.last_mut().unwrap().push(Block::Collection(finished));
                     }
+                    skip = false;
                 }
                 ';' => {
                     if !current_word.is_empty() {
@@ -59,15 +62,32 @@ pub fn parse_text(file_path: &String) -> Result<Vec<Block>, String> {
                     current_multiple_items.push(current_line.clone());
                     current_line.clear();
                     first_char = false;
+                    skip = false;
                 }
                 '\t' | ' ' => {
                     if first_char {
                         current_line.push(current_word.clone());
                         current_word.clear();
                     }
+                    skip = false;
                 }
-                _ => { current_word.push(letters); first_char = true }
+                '/' => {
+                    if skip {
+                        break;
+                    }
+                    skip = true;
+                }
+                _ => { 
+                    current_word.push(letters); 
+                    first_char = true;
+                    skip = false;
+                }
             }
+        }
+
+        if !current_word.is_empty() {
+            current_line.push(current_word.clone());
+            current_word.clear();
         }
 
         if !current_line.is_empty() {
@@ -88,10 +108,16 @@ pub fn parse_text(file_path: &String) -> Result<Vec<Block>, String> {
 }
 
 fn add_last_block(first_char: &mut bool, current_word: &mut String, current_line: &mut Vec<String>, 
-    current_multiple_items: &mut Vec<Vec<String>>, stack: &mut Vec<Vec<Block>>, line: usize, chars: usize) -> Result<(), String>{
+    current_multiple_items: &mut Vec<Vec<String>>, stack: &mut Vec<Vec<Block>>, line: usize, chars: usize, new_line: bool) -> Result<(), String>{
         
     if !current_word.is_empty() {
-        return Err(format!("You haven't finished the line. Line: {}; Char: {}", line, chars));
+        if new_line {
+            current_line.push(current_word.to_string());
+            current_word.clear();
+            *first_char = false;
+        } else {
+            return Err(format!("You haven't finished the line. Line: {}; Char: {}", line, chars));
+        }
     }
     
     if !current_line.is_empty() {
--- src/semantic_analyzer.rs
+++ src/semantic_analyzer.rs
@@ -16,6 +16,8 @@ enum TokenType {
     EQUALS,
     
     NUMBER,
+
+    FN,
     
     UNKNOW,
 }
@@ -25,13 +27,17 @@ impl TokenType {
         match s {
             "if" => TokenType::IF,
             "else" => TokenType::ELSE,
+
             "int" => TokenType::INT,
             "float" => TokenType::FLOAT,
             "str" => TokenType::STRING,
             "bool" => TokenType::BOOL,
+
             "+" => TokenType::PLUS,
             "-" => TokenType::MINUS,
             "=" => TokenType::EQUALS,
+
+            "fn" => TokenType::FN,
 
             _ => TokenType::UNKNOW,
         }
@@ -46,6 +52,14 @@ struct Variable {
 }
 
 impl Types for Variable {
+    fn new(token: TokenType) -> Self {
+        Self {
+            token_type: token,
+            value: None,
+            name: None,
+        }
+    }
+
     fn is_valid_argument(arg: TokenType) -> bool {
          matches!(arg, TokenType::UNKNOW)
     }
@@ -66,8 +80,36 @@ impl Types for Variable {
 #[derive(Debug)]
 struct Function {
     token_type: TokenType,
-    parameters: Vec<TokenType>,
+    parameters: Vec<String>,
     name: Option<String>,
+    table: Vec<TableTypes>,
+}
+
+impl Types for Function {
+    fn new(token: TokenType) -> Self {
+        Self {
+            token_type: token,
+            parameters: vec![],
+            name: None,
+            table: vec![],
+        }
+    }
+
+    fn is_valid_argument(arg: TokenType) -> bool {
+         matches!(arg, TokenType::UNKNOW)
+    }
+
+    fn finished_definition(&self) -> bool {
+        self.name.is_some()
+    }
+
+    fn add_arguments(&mut self, argument: String) {
+        if self.name == None {
+            self.name = Some(argument);
+        } else {
+            self.parameters.push(argument);
+        }
+    }
 }
 
 #[derive(Debug)]
@@ -79,6 +121,7 @@ enum TableTypes {
 }
 
 trait Types {
+    fn new(token: TokenType) -> Self;
     fn is_valid_argument(arg: TokenType) -> bool;
     fn finished_definition(&self) -> bool;
     fn add_arguments(&mut self, argument: String);
@@ -87,7 +130,8 @@ trait Types {
 impl TableTypes {
     fn from_token(token: TokenType) -> Self{
         match token {
-            TokenType::INT | TokenType::FLOAT | TokenType::STRING | TokenType::BOOL => TableTypes::VARIABLE(Variable { token_type: token, value: None, name: None}),
+            TokenType::INT | TokenType::FLOAT | TokenType::STRING | TokenType::BOOL => TableTypes::VARIABLE(Variable::new(token)),
+            TokenType::FN => TableTypes::FUNCTION(Function::new(token)),
             TokenType::IF | TokenType::ELSE => TableTypes::CONDITIONAL,
             _ => TableTypes::ARGUMENT,
         }
@@ -96,6 +140,7 @@ impl TableTypes {
     fn finished_definition(&self) -> bool {
         match self {
             TableTypes::VARIABLE(var) => var.finished_definition(),
+            TableTypes::FUNCTION(fun) => fun.finished_definition(),
             _ => {true},
         }
     }
@@ -103,6 +148,7 @@ impl TableTypes {
     fn add_arguments(&mut self, argument: String) {
         match self {
             TableTypes::VARIABLE(var) => { var.add_arguments(argument)}
+            TableTypes::FUNCTION(fun) => { fun.add_arguments(argument)}
             _ => {}
         }
     }
@@ -113,6 +159,7 @@ struct SemanticAnalyzer {
     error_messages: Vec<String>,
     set_value: bool,
     last_resolved_index: Option<usize>,
+    defining_fn: bool,
 }
 
 impl SemanticAnalyzer {
@@ -122,6 +169,7 @@ impl SemanticAnalyzer {
             error_messages: vec![],
             set_value: false,
             last_resolved_index: None,
+            defining_fn: false,
         }
     }
 
@@ -140,14 +188,18 @@ impl SemanticAnalyzer {
                     }
                 }
                 Block::Collection(blocks) => {
+                    self.defining_fn = matches!(self.active_table().last(), Some(TableTypes::FUNCTION(_)));
+
                     self.analyze(blocks.to_vec());
+
+                    self.defining_fn = false;
                 }
             }
         }
     } 
 
     fn resolve(&mut self, name: String) -> Option<usize> {
-        self.table.iter().position(|entry| match entry {
+        self.active_table().iter().position(|entry| match entry {
             TableTypes::VARIABLE(v) => v.name == Some(name.clone()),
             TableTypes::FUNCTION(f) => f.name == Some(name.clone()),
             _ => false,
@@ -156,7 +208,16 @@ impl SemanticAnalyzer {
 
     fn add_entry(&mut self, token: TokenType) {
         let table_type: TableTypes = TableTypes::from_token(token);
-        self.table.push(table_type)
+        self.active_table().push(table_type)
+    }
+
+    fn active_table(&mut self) -> &mut Vec<TableTypes> {
+        if self.defining_fn {
+            if let Some(TableTypes::FUNCTION(f)) = self.table.last_mut() {
+                return unsafe { &mut *((&mut f.table) as *mut _) };
+            }
+        }
+        &mut self.table
     }
 
     fn tokenize_word(&mut self, word: String) {
@@ -168,35 +229,31 @@ impl SemanticAnalyzer {
         }
 
         let index = self.resolve(word.clone());
-        let is_known = token != TokenType::UNKNOW || index != None;
+        let is_known = token != TokenType::UNKNOW || index.is_some();
+        let last_finished = self.active_table().last().map_or(true, |e| e.finished_definition());
 
-        if let Some(last_entry) = self.table.last_mut() {
-            if !last_entry.finished_definition() || self.set_value {
-                if let Some(idx) = self.last_resolved_index {
-                    self.table[idx].add_arguments(word);
-                    self.last_resolved_index = None;
-                } else {
-                    last_entry.add_arguments(word);
-                }
-                self.set_value = false;
-
-            } else {
-                if token != TokenType::UNKNOW {
-                    self.add_entry(token);
-                } else if is_known {
-                    self.set_value = true;
-                    self.last_resolved_index = index;
-                } else {
-                    self.error_messages.push(format!("Undefined symbol: '{}'", word));
-                }
-            }
-        } else {
+        if self.active_table().is_empty() {
             if token == TokenType::UNKNOW {
                 self.error_messages.push(format!("Tried to start the programm with an unknow word {}", word));
-            } 
-
+            }
             self.add_entry(token);
-            return;
+        } else if !last_finished || self.set_value {
+            if let Some(idx) = self.last_resolved_index {
+                self.active_table()[idx].add_arguments(word);
+                self.last_resolved_index = None;
+            } else {
+                self.active_table().last_mut().unwrap().add_arguments(word);
+            }
+            self.set_value = false;
+        } else {
+            if token != TokenType::UNKNOW {
+                self.add_entry(token);
+            } else if is_known {
+                self.set_value = true;
+                self.last_resolved_index = index;
+            } else {
+                self.error_messages.push(format!("Undefined symbol: '{}'", word));
+            }
         }
     }
 
